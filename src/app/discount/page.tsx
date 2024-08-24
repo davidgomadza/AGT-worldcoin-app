c89cda8eaec5045810364bd14703f79f7962b119
paul.sizon <paul.sizon@outlook.com>
2024-08-24 17:05:54 -0300

    vercel redeploy

--- src/app/discount/page.tsx
+++ src/app/discount/page.tsx
@@ -21,7 +21,7 @@ const DiscountContent: React.FC = () => {
     <div className="flex flex-col justify-center items-center h-screen bg-bg p-4">
       <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full text-center">
         <h1 className="text-2xl font-bold mb-4 text-black">Your Reward</h1>
-        <p className="text-lg mb-8 text-gray-500">{getDiscountMessage()}</p>
+        <p className="text-lg mb-8 text-gray-600">{getDiscountMessage()}</p>
         <button
           className="btn btn-black text-white w-full"
           onClick={() => alert('Discount Applied!')}
