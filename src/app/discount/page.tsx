7a7bd44fddcd4003a1d286c8e3a5954c0d4173b0
paul.sizon <paul.sizon@outlook.com>
2024-08-24 15:24:53 -0300

    vercel fix discount

--- src/app/discount/page.tsx
+++ src/app/discount/page.tsx
@@ -4,7 +4,7 @@
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
-export default function DiscountPage() {
+const DiscountContent: React.FC = () => {
   const searchParams = useSearchParams();
   const choice = searchParams.get('choice');
 
@@ -18,19 +18,25 @@ export default function DiscountPage() {
   };
 
   return (
-    <Suspense>
-      <div className="flex flex-col justify-center items-center h-screen bg-bg p-4">
-        <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full text-center">
-          <h1 className="text-2xl font-bold mb-4 text-black">Your Reward</h1>
-          <p className="text-lg mb-8 text-gray-500">{getDiscountMessage()}</p>
-          <button
-            className="btn btn-black text-white w-full"
-            onClick={() => alert('Discount Applied!')}
-          >
-            Apply Discount
-          </button>
-        </div>
+    <div className="flex flex-col justify-center items-center h-screen bg-bg p-4">
+      <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full text-center">
+        <h1 className="text-2xl font-bold mb-4 text-black">Your Reward</h1>
+        <p className="text-lg mb-8 text-gray-500">{getDiscountMessage()}</p>
+        <button
+          className="btn btn-black text-white w-full"
+          onClick={() => alert('Discount Applied!')}
+        >
+          Apply Discount
+        </button>
       </div>
+    </div>
+  );
+};
+
+export default function DiscountPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DiscountContent />
     </Suspense>
   );
 }
