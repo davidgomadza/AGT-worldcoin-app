5a984507986821e5d3f68bedf67224e789c85e5f
paul.sizon <paul.sizon@outlook.com>
2024-08-24 15:14:02 -0300

    vercel fix

--- src/app/ad/page.tsx
+++ src/app/ad/page.tsx
@@ -2,10 +2,10 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-const Advertisement: React.FC = () => {
+const AdContent: React.FC = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
 
@@ -16,15 +16,21 @@ const Advertisement: React.FC = () => {
     };
 
     return (
-        <Suspense>
-            <div className="flex flex-col justify-center items-center h-screen bg-bg text-center">
-                <h1 className="text-2xl font-bold mb-8 text-black">
-                    Watch an advertisement to get a discount at Adidas Shop
-                </h1>
-                <button className="btn btn-black text-white w-64" onClick={handleWatchAd}>
-                    Yes
-                </button>
-            </div>
+        <div className="flex flex-col justify-center items-center h-screen bg-bg text-center">
+            <h1 className="text-2xl font-bold mb-8 text-black">
+                Watch an advertisement to get a discount at Adidas Shop
+            </h1>
+            <button className="btn btn-black text-white w-64" onClick={handleWatchAd}>
+                Yes
+            </button>
+        </div>
+    );
+};
+
+const Advertisement: React.FC = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <AdContent />
         </Suspense>
     );
 };
